9c90071fdc197d0bc4cae690309673a0bb4be30f
hetmanPpc <67745359+hetmanPpc@users.noreply.github.com>
2021-08-14 13:11:38 +0200

    Extending sruff in LogHandler.
    Extended :
    - IO
    - addLog
    Fixing :
    - LogDisplay

---
+++ Simple.java
@@ -0,0 +1,26 @@
+package SimpleProject;
+
+import Xirta.LogHandler;
+
+public class Simple {
+/*
+*                   *
+*   Testing Class   *
+*                   *
+*/
+
+
+
+    public static void main(String[] args) {
+
+       Xirta.LogHandler.IO(money(5),5,"Money");
+
+
+
+    }
+
+    public static int money(int x){
+    //    x+=5;
+        return x;
+    }
+}
